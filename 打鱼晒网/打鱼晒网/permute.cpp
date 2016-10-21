91fcceb6e556b9b17e5db65133f5a5febdd399fd
xiaojin31 <2996039092@qq.com>
2016-10-21 22:26:35 +0800

    no message

---
+++ 打鱼晒网/打鱼晒网/permute.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+void f(int &x,int &y);
+void g(int &x,int &y);
+int main(){
+	std::cout<<"Input two numbers:"<<std::endl;
+	int a,b;
+	std::cin>>a>>b;
+	f(a,b);
+	std::cout<<"Fist permute:"<<a<<" "<<b<<std::endl;
+	g(a,b);
+	std::cout<<"Second permute:"<<a<<" "<<b<<std::endl;
+}
+void f(int &x,int &y)
+{
+	int t = x;
+	x = y;
+	y = t;
+}
+void g(int &x,int &y)
+{
+	int t = x+y;
+	y = x;
+	x = t-y;
+}
