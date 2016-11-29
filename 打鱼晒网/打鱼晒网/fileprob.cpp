f69ed004f63802db84b13004cc04cec3186ff355
xiaojin31 <2996039092@qq.com>
2016-11-29 16:14:20 +0800

    no message

--- 打鱼晒网/打鱼晒网/fileprob.cpp
+++ 打鱼晒网/打鱼晒网/fileprob.cpp
@@ -5,12 +5,31 @@
 
 using namespace std;
 
-int countLinesNum(char *filename);
+int countLinesNum(const char *filename);  // note1: 函数不修改形参的话，把形参声明为const
 void printLongAndShortLine(char *filename);
+
+/*int countLinesNum2(const char *filename) {
+    // filename[0] = 'u';
+}*/
 
 int main()
 {
-	printLongAndShortLine("D:\\test\\file1.txt");
+
+    //ifstream is;
+    //ofstream os;
+
+    // char name[] = "hello";
+    // countLinesNum2(name);
+    // cout << name << endl;
+
+    // const -> non-const
+    // non-const -> const
+
+    //const char *fileName = "t.txt";
+    //countLinesNum(fileName);
+
+	 printLongAndShortLine("D:\\test\\file1.txt");
+     printLongAndShortLine("file1.txt");
 }
 
 void printLongAndShortLine(char *filename)
@@ -18,15 +37,22 @@ void printLongAndShortLine(char *filename)
 	int k = 0,p = 0;
 	char a[20][100];
 	int b[20];
-	fstream f1;
+	// fstream f1;  // note2
+    ifstream f1; 
 	f1.open(filename);
+    /*
+     * 
+     * 
+     * void open( const char *filename, ios_base::openmode mode = ios_base::in );
+     * 
+     */
 	int s = countLinesNum(filename);
 	for(int i =0 ; i<s; ++i)
 	{
 		f1.getline(a[i],100,'\n');
 		b[i] = strlen(a[i]);
 	}
-	for(int i = 1; i < s-1; ++i)
+	for(int i = 1; i < s; ++i) // error1
 	{
 		if( b[k] > b[i] )
 		{
@@ -34,7 +60,7 @@ void printLongAndShortLine(char *filename)
 		}
 	}
 	cout << "short:" << a[k] << endl;
-	for(int i = 1; i < s-1; ++i)
+	for(int i = 1; i < s; ++i) // error1
 	{
 		if( b[p] < b[i])
 		{
@@ -45,7 +71,7 @@ void printLongAndShortLine(char *filename)
 	f1.close();
 }
 
-int countLinesNum(char *filename)
+int countLinesNum(const char *filename)
 {
 	ifstream file1;
 	file1.open(filename);
